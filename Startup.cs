5429fea13dabd7656af0255bc160a5b8517f41f6
damduyr <114305300+damduyr@users.noreply.github.com>
2025-08-05 18:34:37 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Streamverse.Startup))]
+namespace Streamverse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
